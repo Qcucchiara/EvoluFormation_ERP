8b4fe5f3043e5ae865bc4e40b46da036bffa6244
Phost <cucchiaraquentin@gmail.com>
2024-10-04 16:17:14 +0200

    Session mongoose import

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
-import { SessionService } from './session.service';
-import { SessionController } from './session.controller';
+import { Module } from "@nestjs/common";
+import { SessionService } from "./session.service";
+import { SessionController } from "./session.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Session, SessionSchema } from "src/schemas/Session.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Session.name, schema: SessionSchema }]),
+  ],
   controllers: [SessionController],
   providers: [SessionService],
 })
